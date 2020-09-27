8b7f567cbdfa8033f52c033f12f5a50072feda58
iReekHavok <bcagampang96@gmail.com>
2020-09-27 16:01:35 -0500

    Finished dice game

---
+++ dice.js
@@ -0,0 +1,7 @@
+function RollTwentySidedDice1() {
+    document.getElementById("dice1").value = Math.floor(Math.random()*20) + 1;
+}
+
+function RollTwentySidedDice2() {
+    document.getElementById("dice2").value = Math.floor(Math.random()*20) + 1;
+}
